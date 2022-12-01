bd8d5b451f24fa67740ab665342452179ba0a6c0
Ozioma Ogbe <ozioma.ogbe@agoda.com>
2022-12-01 15:35:05 +0700

    Add solution for day 1 2022

--- solutions/src/year_2022/day01/Day1_1.kt
+++ solutions/src/year_2022/day01/Day1_1.kt
@@ -3,5 +3,9 @@ package year_2022.day01
 import solve
 
 fun main() = solve { lines ->
-
+    lines.joinToString("\n").split("\n\n").map {
+        it.split('\n').map { it.toLong() }
+    }.maxOfOrNull {
+        it.sum()
+    }!!
 }
--- solutions/src/year_2022/day01/Day1_2.kt
+++ solutions/src/year_2022/day01/Day1_2.kt
@@ -1,7 +1,11 @@
-package day01
+package year_2022.day01
 
 import solve
 
 fun main() = solve { lines ->
-
+    lines.joinToString("\n").split("\n\n").map {
+        it.split('\n').map { it.toLong() }
+    }.map {
+        it.sum()
+    }.sorted().takeLast(3).sum()
 }
