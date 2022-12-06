837d0cb7165b0ef8b5c2afb95dcb390fd0c8b309
Ozioma Ogbe <ozioma.ogbe@agoda.com>
2022-12-06 11:04:44 +0700

    Add day 5 solution

--- solutions/src/year_2022/day05/Day5_1.kt
+++ solutions/src/year_2022/day05/Day5_1.kt
@@ -1,7 +1,48 @@
-package day05
+package year_2022.day05
 
 import solve
+import java.util.LinkedList
+import java.util.Queue
+import java.util.Stack
+
+data class Instruction(val source: Int, val destination: Int, val amount: Int)
 
 fun main() = solve { lines ->
-
+    parseInput(lines).apply {
+        second.forEach { instruction ->
+            (1..instruction.amount).forEach {
+                first[instruction.destination - 1].push(first[instruction.source - 1].pop())
+            }
+        }
+        first.forEach {
+            if (it.isNotEmpty()) print(it.peek()[1])
+        }
+    }
 }
+
+fun parseInput(lines: List<String>): Pair<List<Stack<String>>, List<Instruction>> {
+    val (stacks, instructionString) = lines.joinToString("\n").split("\n\n")
+    return List(10) { LinkedList<String>() }.apply {
+        stacks.split("\n").take(stacks.split("\n").size - 1).forEach { value ->
+            value.chunked(4).forEachIndexed { index, s ->
+                if (s.isNotBlank()) {
+                    this[index].add(s)
+                }
+            }
+        }
+    }.mapIndexed { index, strings ->
+        val stack = Stack<String>()
+        strings.reversed().forEach {
+            stack.push(it)
+        }
+        stack
+    } to instructionString.split("\n").map {
+        it.split(" ").mapNotNull {
+            it.toIntOrNull()
+        }
+    }.map {
+        Instruction(it[1], it[2], it[0])
+    }
+}
+
+
--- solutions/src/year_2022/day05/Day5_2.kt
+++ solutions/src/year_2022/day05/Day5_2.kt
@@ -1,7 +1,21 @@
-package day05
+package year_2022.day05
 
 import solve
+import java.util.*
 
 fun main() = solve { lines ->
-
+    parseInput(lines).apply {
+        second.forEach { instruction ->
+            val temp = mutableListOf<String>()
+            (1..instruction.amount).forEach {
+                temp.add(first[instruction.source - 1].pop())
+            }
+            temp.reversed().forEach {
+                first[instruction.destination - 1].push(it)
+            }
+        }
+        first.forEach {
+            if (it.isNotEmpty()) print(it.peek()[1])
+        }
+    }
 }
