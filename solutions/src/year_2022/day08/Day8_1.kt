fd3ea3894ccd28156967e873fce265c7dd88c90d
Ozioma Ogbe <ozioma.ogbe@agoda.com>
2022-12-08 20:31:34 +0700

    Add solution for day 8.

--- solutions/src/year_2022/day08/Day8_1.kt
+++ solutions/src/year_2022/day08/Day8_1.kt
@@ -1,7 +1,49 @@
-package day08
+package year_2022.day08
 
 import solve
 
 fun main() = solve { lines ->
-
+    val grid = lines.mapIndexed { x, trees ->
+        trees.mapIndexed { y, tree ->
+            Tree(tree.digitToInt(), x, y)
+        }
+    }
+    (getVisibleTrees(grid) + getVisibleTrees(grid.transpose {
+        Tree(-1, -1, -1)
+    })).size
 }
+
+private fun getVisibleTrees(trees: List<List<Tree>>): Set<Tree> {
+    return trees.map { treeRow ->
+        treeRow.takeLast(treeRow.size - 1).fold(listOf(treeRow.first())) { visibleTrees, tree ->
+            foldTrees(visibleTrees, tree)
+        } + treeRow.take(treeRow.size - 1).reversed().fold(listOf(treeRow.last())) { visibleTrees, tree ->
+            foldTrees(visibleTrees, tree)
+        }
+    }.flatten().toSet()
+}
+
+private fun foldTrees(visibleTrees: List<Tree>, tree: Tree): List<Tree> {
+    return if (tree.height > visibleTrees.maxBy { it.height }.height) {
+        buildList {
+            addAll(visibleTrees + tree)
+        }
+    } else {
+        visibleTrees
+    }
+}
+
+data class Tree(val height: Int, val positionX: Int, val positionY: Int)
+
+fun <T> List<List<T>>.transpose(initializer: () -> T): List<List<T>> {
+    return List(this.first().size) {
+        List(this.size) {
+            initializer()
+        }
+    }.mapIndexed { x, ts ->
+        List(ts.size) { y ->
+            this[y][x]
+        }
+    }
+}
+
--- solutions/src/year_2022/day08/Day8_2.kt
+++ solutions/src/year_2022/day08/Day8_2.kt
@@ -1,7 +1,51 @@
-package day08
+package year_2022.day08
 
 import solve
 
 fun main() = solve { lines ->
+    val grid = lines.mapIndexed { x, trees ->
+        trees.mapIndexed { y, tree ->
+            Tree(tree.digitToInt(), x, y)
+        }
+    }
 
+    grid.forEachIndexed { index, trees ->
+        trees.forEachIndexed { index1, tree ->
+            findDistanceForTree(trees, index1)
+        }
+        trees.reversed().forEachIndexed { index1, tree ->
+            findDistanceForTree(trees.reversed(), index1)
+        }
+    }
+    grid.transpose {
+        Tree(-1, -1, -1)
+    }.forEachIndexed { index, trees ->
+        trees.forEachIndexed { index1, tree ->
+            findDistanceForTree(trees, index1)
+        }
+        trees.reversed().forEachIndexed { index1, tree ->
+            findDistanceForTree(trees.reversed(), index1)
+        }
+    }
+    cache.maxBy {
+        it.value
+    }
+}
+
+val cache = mutableMapOf<Tree, Int>()
+
+private fun findDistanceForTree(
+    trees: List<Tree>, indexY: Int
+) {
+    var result = 0
+    if (indexY < trees.lastIndex) {
+        var copy = indexY
+        while (copy < trees.lastIndex && trees[copy + 1].height < trees[indexY].height) {
+            result++
+            copy++
+        }
+        if (copy < trees.lastIndex) result++
+    }
+
+    cache[trees[indexY]] = (cache[trees[indexY]] ?: 1) * result
 }
