bcb24aac1fa21b60207500a2e5e2666ca5172765
Ozioma Ogbe <ozioma.ogbe@agoda.com>
2022-12-09 08:51:04 +0700

    Use heap for part 1 day 8

--- solutions/src/year_2022/day08/Day8_1.kt
+++ solutions/src/year_2022/day08/Day8_1.kt
@@ -1,6 +1,7 @@
 package year_2022.day08
 
 import solve
+import java.util.PriorityQueue
 
 fun main() = solve { lines ->
     val grid = lines.mapIndexed { x, trees ->
@@ -15,25 +16,32 @@ fun main() = solve { lines ->
 
 private fun getVisibleTrees(trees: List<List<Tree>>): Set<Tree> {
     return trees.map { treeRow ->
-        treeRow.takeLast(treeRow.size - 1).fold(listOf(treeRow.first())) { visibleTrees, tree ->
-            foldTrees(visibleTrees, tree)
-        } + treeRow.take(treeRow.size - 1).reversed().fold(listOf(treeRow.last())) { visibleTrees, tree ->
-            foldTrees(visibleTrees, tree)
-        }
+        treeRow.takeLast(treeRow.size - 1)
+            .fold(PriorityQueue<Tree>().also { it.add(treeRow.first()) }) { visibleTrees, tree ->
+                foldTrees(visibleTrees, tree)
+            } + treeRow.take(treeRow.size - 1).reversed()
+            .fold(PriorityQueue<Tree>().also { it.add(treeRow.last()) }) { visibleTrees, tree ->
+                foldTrees(visibleTrees, tree)
+            }
     }.flatten().toSet()
 }
 
-private fun foldTrees(visibleTrees: List<Tree>, tree: Tree): List<Tree> {
-    return if (tree.height > visibleTrees.maxBy { it.height }.height) {
-        buildList {
-            addAll(visibleTrees + tree)
+private fun foldTrees(visibleTrees: PriorityQueue<Tree>, tree: Tree): PriorityQueue<Tree> {
+    return if (tree.height > visibleTrees.last().height) {
+        PriorityQueue<Tree>().also {
+            it.addAll(visibleTrees + tree)
         }
     } else {
         visibleTrees
     }
 }
 
-data class Tree(val height: Int, val positionX: Int, val positionY: Int)
+data class Tree(val height: Int, val positionX: Int, val positionY: Int) : Comparable<Tree> {
+    override fun compareTo(other: Tree): Int {
+        return this.height.compareTo(other.height)
+    }
+
+}
 
 fun <T> List<List<T>>.transpose(initializer: () -> T): List<List<T>> {
     return List(this.first().size) {
